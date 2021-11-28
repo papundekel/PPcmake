f5e988849d366b0f64937c7c983ccc58410d906f
Peter Fačko <pfacko1@gmail.com>
2021-11-28 13:49:12 +0100

    RC 1

--- PPcmake.cmake
+++ PPcmake.cmake
@@ -1,18 +1,73 @@
-cmake_minimum_required(VERSION 3.20)
+cmake_minimum_required(VERSION 3.22)
 
-function(PPcmake_init)
-    include(GNUInstallDirs)
-    set(PPCMAKE_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/cmake" PARENT_SCOPE)
-    set(PPCMAKE_VENDORDIR "${CMAKE_CURRENT_LIST_DIR}/vendor")
-    set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH};${PPCMAKE_VENDORDIR}/${PPCMAKE_INSTALL_CMAKEDIR}" PARENT_SCOPE)
-endfunction()
+include(GNUInstallDirs)
 
-function(PPcmake_download_package _URL)
-    cmake_path(GET _URL FILENAME _filename)
+find_package(Git)
+if(NOT Git_FOUND)
+    message(FATAL_ERROR "Git not found")
+endif()
 
-    file(DOWNLOAD "${URL}" "${_filename}")
-    file(ARCHIVE_EXTRACT INPUT "${_filename}" DESTINATION "${CMAKE_CURRENT_LIST_DIR}/vendor")
-    file(REMOVE "${_filename}")
+#
+
+set(PPCMAKE_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/cmake")
+set(PPCMAKE_VENDORDIR "${CMAKE_SOURCE_DIR}/vendor")
+set(CMAKE_MODULE_PATH "${PPCMAKE_VENDORDIR}/${PPCMAKE_INSTALL_CMAKEDIR}")
+set(PPCMAKE_PACKAGES_DIR "${CMAKE_BINARY_DIR}/packages/")
+
+#
+
+file(MAKE_DIRECTORY "${PPCMAKE_PACKAGES_DIR}")
+
+#
+
+function(PPcmake_package _GIT_SERVER _USER _REPOSITORY _TAG)
+    set(_repo_dir_src "${PPCMAKE_PACKAGES_DIR}/${_REPOSITORY}")
+    set(_repo_dir_out "${_repo_dir_src}/out-cmake")
+
+    if(EXISTS "${_repo_dir_src}")
+        message("${_REPOSITORY} already found.")
+        return()
+    endif()
+
+    execute_process(
+        COMMAND "${GIT_EXECUTABLE}"
+            "clone"
+            "--branch" "${_TAG}"
+            "--depth" "1"
+            "https://${_GIT_SERVER}/${_USER}/${_REPOSITORY}"
+            "${_repo_dir_src}"
+        OUTPUT_FILE "${PPCMAKE_PACKAGES_DIR}/output.log"
+        ERROR_FILE "${PPCMAKE_PACKAGES_DIR}/errors.log"
+        COMMAND_ECHO STDOUT
+    )
+    execute_process(
+        COMMAND "${CMAKE_COMMAND}"
+            "-S" "${_repo_dir_src}"
+            "-B" "${_repo_dir_out}"
+            "-G" "Ninja Multi-Config"
+        OUTPUT_FILE "${PPCMAKE_PACKAGES_DIR}/output.log"
+        ERROR_FILE "${PPCMAKE_PACKAGES_DIR}/errors.log"
+        COMMAND_ECHO STDOUT
+    )
+    execute_process(
+        COMMAND "${CMAKE_COMMAND}"
+            "--build" "${_repo_dir_out}"
+            "--config" "Release"
+            "--target all"
+            "-j" "10"
+        OUTPUT_FILE "${PPCMAKE_PACKAGES_DIR}/output.log"
+        ERROR_FILE "${PPCMAKE_PACKAGES_DIR}/errors.log"
+        COMMAND_ECHO STDOUT
+    )
+    execute_process(
+        COMMAND "${CMAKE_COMMAND}"
+            "--install" "${_repo_dir_out}"
+            "--config" "Release"
+            "--prefix" "${CMAKE_SOURCE_DIR}/vendor/"
+        OUTPUT_FILE "${PPCMAKE_PACKAGES_DIR}/output.log"
+        ERROR_FILE "${PPCMAKE_PACKAGES_DIR}/errors.log"
+        COMMAND_ECHO STDOUT
+    )
 endfunction()
 
 function(PPcmake_reset_notfound_var _LIST)
@@ -34,4 +89,18 @@ function(PPcmake_install_file _FILEPATH)
     endif()
 
     install(FILES "${_FILEPATH}" DESTINATION "${_dest}")
+endfunction()
+
+function(PPcmake_target_pedantic_errors _target)
+	if(CMAKE_COMPILER_IS_GNUCXX)
+		target_compile_options("${_target}"
+			PUBLIC "-Wall" "-Wextra" "-pedantic" "-Werror"
+		)
+	endif()
+endfunction()
+
+function(PPcmake_add_subdirectory _DIR)
+    if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/${_DIR}/")
+        add_subdirectory("${_DIR}")
+    endif()
 endfunction()
